1845eac0bbda94d4dcfb5badc17ee487ec80d702
Andision <48558940+Andision@users.noreply.github.com>
2022-10-21 15:24:06 +0800

    Update utils.py

--- py/utils.py
+++ py/utils.py
@@ -1,9 +1,22 @@
+import time
+
+def myPrintLog(tag, *args):
+    for i in args:
+        print("[{time}]    {tag}    {msg}".format(
+            time=time.strftime("%H:%M:%S"), msg=i, tag=tag.rjust(20)))
+
+
 def AES_Encryption(content, key):
-    return content+key
+    return content+'#'+key
 
 
 def AES_Decryption(content, key):
-    pass
+    return content.split('#')[0]
 
-def Prefix_Verify(content,prefix):
-    return True
+def keyGenerate(len=32):
+    '''Generate key for encryption. Default length is 32 bytes (256 bits).
+
+    :param len: The length of key, default length is 32 bytes (256 bits).
+
+    '''
+    return "key_gen_for_data_transfer"+time.strftime("%H:%M:%S")
